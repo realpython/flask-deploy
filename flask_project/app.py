9482e31e68af19d113a8043956bcec4c118c2c52
Michael Herman <hermanmu@gmail.com>
2014-10-25 11:05:20 -0500

    initial

---
+++ flask_project/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Flask is running!'
+
+
+@app.route('/data')
+def names():
+    data = {
+        "first_names": ["John", "Jacob", "Julie", "Jennifer"]
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
